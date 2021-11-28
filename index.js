d70c987c4d62a707673bef597f003bce003d13a6
TG2408 <tanmaygupta2408@gmail.com>
2021-11-28 16:02:48 +0530

    Cors handled

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Packages import
 // const express = require('express');
 import express from "express";
+import cors from 'cors';
+import bodyParser from "body-parser";
 
 //Components import
 import connection from "./connection/db.js";
@@ -9,6 +11,9 @@ import Route from "./routes/route.js";
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
 app.use('/', Route);
 
 const PORT = 8000;
