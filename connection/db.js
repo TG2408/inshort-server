4174c3d9fa85b4f811ef432cb018954e5a6dd0f9
TG2408 <tanmaygupta2408@gmail.com>
2021-12-09 07:41:07 +0530

    minor changes

--- connection/db.js
+++ connection/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 const connection = async () => {
     try {
-        const URL = `mongodb+srv://tanmaygupta24:abcd1234@inshortclone.2vn0b.mongodb.net/INSHORTSCLONE?retryWrites=true&w=majority`;
+        const URL = `mongodb+srv://tanmaygupta24:niec1234@inshortclone.2vn0b.mongodb.net/INSHORTSCLONE?retryWrites=true&w=majority`;
 
         await mongoose.connect(URL, { useNewUrlParser: true });
 
